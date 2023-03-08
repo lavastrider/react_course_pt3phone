b4415d73addb76a4416ce5df8b66200cbd6f9634
lavastrider <70551407+lavastrider@users.noreply.github.com>
2023-03-08 14:34:57 -0600

    Add files via upload

---
+++ model/Name.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const nameSchema = new mongoose.Schema({
+  phoneName: String,
+  phoneNumber: String
+})
+
+nameSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Name', nameSchema)
